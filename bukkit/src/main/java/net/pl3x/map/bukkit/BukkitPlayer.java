e1dc635a87f14dee0a89756f9a5572f3a7ccfb12
U5B <56985400+U5B@users.noreply.github.com>
2024-02-12 19:32:29 -0800

    Revert "update to 1.20.2"
    This reverts commit d576561d033ce8d0e60e08c6d0638a5dc6b0cfe0.
    
    Signed-off-by: U5B <56985400+U5B@users.noreply.github.com>

--- bukkit/src/main/java/net/pl3x/map/bukkit/BukkitPlayer.java
+++ bukkit/src/main/java/net/pl3x/map/bukkit/BukkitPlayer.java
@@ -108,7 +108,7 @@ public class BukkitPlayer extends Player {
             if (property == null) {
                 return null;
             }
-            String json = new String(Base64.getDecoder().decode(property.value()), StandardCharsets.UTF_8);
+            String json = new String(Base64.getDecoder().decode(property.getValue()), StandardCharsets.UTF_8);
             JsonElement jsonElement = JsonParser.parseString(json);
             if (!jsonElement.isJsonObject()) {
                 return null;
